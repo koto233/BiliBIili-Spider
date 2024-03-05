ecd77732ea3368627e7a3df7e0378e9b32eb6460
koto233 <1768733431@qq.com>
2024-03-05 18:17:45 +0800

    api方法获取评论

--- Code/test.py
+++ Code/test.py
@@ -1,9 +1,42 @@
-import pickle
-from browsermobproxy import Server
-from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup
+import json
+from fake_useragent import UserAgent
 
 
-with open("Data\登录文件\cookies文件.pickle", "rb") as file:
-    cookiesList = pickle.load(file)
+def Request_Header():
+    """_summary_
+    浏览器请求头伪装
+        Returns:
+            {}: 浏览器请求头
+    """
+    headers = {"User-Agent": UserAgent().random}
 
-print(cookiesList)
+    return headers
+
+
+headers = {"User-Agent": "XXXX"}
+# 视频id
+oid = 32612698
+# 评论页数
+pn = 1
+# 排序种类 0是按时间排序 2是按热度排序
+sort = 2
+
+while True:
+    url = f"https://api.bilibili.com/x/v2/reply?pn={pn}&type=12&oid={oid}&sort={sort}"
+    reponse = requests.get(url, headers=headers)
+    a = json.loads(reponse.text)
+    print(a)
+    if pn == 1:
+        count = a["data"]["page"]["count"]
+        size = a["data"]["page"]["size"]
+        page = count // size + 1
+        print(page)
+    for b in a["data"]["replies"]:
+        # print(b["content"]["message"])
+        print("-" * 10)
+    if pn != page:
+        pn += 1
+    else:
+        break
